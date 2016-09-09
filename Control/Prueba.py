880240768e4f37029fddd3279bd50280ad4f672a
ramonemiliani93 <rd.emiliani689@uniandes.edu.co>
2016-09-09 16:59:11 -0500

    Prueba con constantes actualizadas

--- Control/Prueba.py
+++ Control/Prueba.py
@@ -1,22 +1,39 @@
-import pid
-import pwm
+import pid as pid
+import pwm as pwm
 import Adafruit_DHT
+import RPi.GPIO as GPIO
 from time import sleep
 import os
 
 sensor = Adafruit_DHT.AM2302
 pin = 23
 
-p = pid.PID(1,1,0.02, Integrator_max=100, Integrator_min=0)
-p.setPoint(29.0)
+p = pid.PID(-60, -0.02, 0, Integrator_max=100, Integrator_min=-100)
+p.setPoint(8)
 
+GPIO.setmode(GPIO.BCM)
 
-pwm_control = pwm.PWM(1000, 0, 0, 12)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(21, GPIO.OUT)
 
-while True:
-    sleep(.5)
-    os.system('clear')
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-    x = (p.update(temperature))*-1
-    print x
-    pwm_control.setCycle(x)
+GPIO.output(16, GPIO.HIGH)
+GPIO.output(21, GPIO.LOW)
+
+fan = pwm.PWM(500, 0, 0, 12)
+fan.startPWM()
+
+try:
+    while True:
+        sleep(.5)
+        os.system('clear')
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+        Ciclo = (p.update(temperature)/24)*100
+        try :
+            fan.setCycle(Ciclo)
+        except :
+            print('No se pudo leer el sensor')
+        print(str(temperature))
+        print(str(Ciclo))
+
+except KeyboardInterrupt:
+  pass
--- Control/pwm.py
+++ Control/pwm.py
@@ -1,13 +1,14 @@
-import RPi.GPIO as GPIO 
+import RPi.GPIO as GPIO
 
 #Set mode for GPIO
+
 
 class PWM:
     """
     Controlador PWM
     """
-    
-    def __init__(self, frequency, cycleStart, cycle, output) :
+
+    def __init__(self, frequency, cycleStart, cycle, output):
 
         self.frequency = frequency
         self.cycleStart = cycleStart
@@ -17,17 +18,18 @@ class PWM:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(output, GPIO.OUT)
 
-        self.PWM_control = GPIO.PWM(output,frequency)
-        
-    def setFrequency(self,new_frequency):
+        self.PWM_control = GPIO.PWM(output, frequency)
+        PWM_control = GPIO.PWM(output, frequency)
+
+    def setFrequency(self, new_frequency):
         self.frequency = new_frequency
         self.PWM_control.ChangeFrequency(new_frequency)
 
     def setCycle(self, new_cycle):
-        if (new_cycle<0):
-            self.cycle=0
-        elif (new_cycle>100):
-            self.cycle=100
+        if (new_cycle < 0):
+            self.cycle = 0
+        elif (new_cycle > 100):
+            self.cycle = 100
         else:
             self.cycle = new_cycle
         self.PWM_control.ChangeDutyCycle(self.cycle)
