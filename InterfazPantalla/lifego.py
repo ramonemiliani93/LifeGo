bcb65cab4109d58e479833316042b23e1d5f59f8
ramonemiliani93 <rd.emiliani689@uniandes.edu.co>
2016-11-22 10:09:13 -0500

    in

--- InterfazPantalla/lifego.py
+++ InterfazPantalla/lifego.py
@@ -4,14 +4,22 @@ sys.path.insert(0, '/home/pi/Documents/LifeGo/LifeGo/Sensores')
 
 import control
 import ambiente
+import organo
 import threading
+from time import sleep
+
+import kivy
+kivy.require('1.9.0')
+
 
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
 from kivy.logger import Logger
 from kivy.clock import Clock
+
 
 Builder.load_file('ajuste.kv')
 Builder.load_file('informacion.kv')
@@ -20,31 +28,62 @@ Builder.load_file('menu.kv')
 
 class LifeGo(AnchorLayout):
     temperaturaDeseada = ObjectProperty()
+    temperaturaActual = StringProperty()
+    humedadActual = StringProperty()
+    temperaturaOrganoActual = StringProperty()
 
     def __init__(self, **kwargs):
         super(LifeGo, self).__init__(**kwargs)
-        self.control = control.Control()
-        self.ambiente = ambiente.TemperaturaAmbiente()
-        self.ambiente.start()
-        threading.Thread(target=control.actualizar, args=(ambiente,)).start()
+        self.controlT = control.Control()
+        self.ambienteS = ambiente.TemperaturaAmbiente()
+        self.organoS = organo.TemperaturaOrgano()
+        
+        self.ambienteS.start()
+        self.organoS.start()
+        threading.Thread(target=self.controlT.actualizar, args=(self.ambienteS,)).start()
+        
+        self.temperaturaActual = str('no ha iniciado')
+        self.humedadActual = str('no ha iniciado')
+        self.temperaturaOrganoActual = str('no ha iniciado')
+        
         self.temperaturaDeseada = 4
-        control.setPoint(self.temperaturaDeseada)
+        self.controlT.setPoint(self.temperaturaDeseada)
 
     def disminuirTemperaturaDeseada(self):
         self.temperaturaDeseada -= 1
-        self.control.setPoint(self.temperaturaDeseada)
+        self.controlT.setPoint(self.temperaturaDeseada)
         Logger.info(str(self.temperaturaDeseada))
 
     def aumentarTemperaturaDeseada(self):
         self.temperaturaDeseada += 1
-        self.control.setPoint(self.temperaturaDeseada)
+        self.controlT.setPoint(self.temperaturaDeseada)
         Logger.info(str(self.temperaturaDeseada))
 
+    def updateInterfaz(self):
+        while True:
+            sleep(5)
+            ttemporal = self.ambienteS.getTemperaturaAmbiente()
+            htemporal = self.ambienteS.getHumedadAmbiente()
+            torganotemporal = self.organoS.getTemperaturaOrgano()
+            if ttemporal is None:
+                self.temperaturaActual = str('...')
+            else:
+                self.temperaturaActual = '{0:.1f}'.format(round(ttemporal, 1))
+            if htemporal is None:
+                self.humedadActual = str('...')
+            else:
+                self.humedadActual = '{0:.1f}'.format(round(htemporal, 1))
+            if torganotemporal is None:
+                self.temperaturaOrganoActual = str('...')
+            else:
+                self.temperaturaOrganoActualhumedadActual = '{0:.1f}'.format(round(torganotemporal, 1))
 
 class LifeGoApp(App):
 
     def build(self):
-        return LifeGo()
+        lf = LifeGo()
+        threading.Thread(target=lf.updateInterfaz).start()
+        return lf
 
 if __name__ == '__main__':
     LifeGoApp().run()
--- Sensores/organo.py
+++ Sensores/organo.py
@@ -18,7 +18,7 @@ class TemperaturaOrgano(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self)
         self.ADDRESS_DEVICE = 0x5A
-        self.ADDRESS_OBJECT = 0x07
+        self.ADDRESS_OBJECT1 = 0x07
         self.sensor = I2C.Device(self.ADDRESS_DEVICE, 1)
         self.corriendo = True
         self.temperturaOrgano = None
@@ -27,10 +27,11 @@ class TemperaturaOrgano(threading.Thread):
         sleep(5)
         try:
             while self.corriendo:
-                ByteA = self.sensor.readU16(self.ADDRESS_OBJECT)
-                temperatura = ByteA/50-273.15
+                ByteA = self.sensor.readS16(self.ADDRESS_OBJECT1)
+                temperatura = (0.02*ByteA)-273.15
                 if temperatura is not None:
                     self.temperturaOrgano = temperatura
+      
         except:
             pass
 
@@ -47,6 +48,6 @@ if __name__ == '__main__':
     temperaturaorgano.start()
 
     while True:
-        sleep(10)
-        print('La temperatura en este momento es {}'.format(temperaturaorgano.temperturaOrgano))
+        sleep(5)
+        print('La temperatura del organo es: {}'.format(temperaturaorgano.temperturaOrgano))
 
